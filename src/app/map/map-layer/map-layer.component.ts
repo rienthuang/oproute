d75df1020334341cfa03bd664719cc2e938dce98
Ryan Huang <ryanhuanglw@gmail.com>
2020-06-12 18:12:55 +0800

    Created a map-layer component to display various available SLA tiles
    These tiles are essentially map layers that can be added onto the map based on a boolean - controlling which layer the user prefers to be displayed.
    
    The available tiles so far are:
    - Default Tile (map is initialized with this tile by default aka the Base Layer)
    - Land Tile (original)
    - Night Tile

---
+++ src/app/map/map-layer/map-layer.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+import { tileLayer } from "leaflet";
+
+@Component({
+  selector: 'app-map-layer',
+  templateUrl: './map-layer.component.html',
+  styleUrls: ['./map-layer.component.css']
+})
+export class MapLayerComponent implements OnInit {
+
+
+  showLandLayer = false;
+
+  landTile = tileLayer('https://maps-{s}.onemap.sg/v3/Original/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    minZoom: 11,
+    attribution: 'Map data &copy contributors, <a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a> &nbsp;&#124;&nbsp; <a href="https://tech.gov.sg/report_vulnerability" target="_blank">Report Vulnerability</a>',
+  });
+
+  showNightLayer = false;
+
+  nightTile = tileLayer('https://maps-{s}.onemap.sg/v3/Night/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    minZoom: 11,
+    attribution: 'Map data &copy contributors, <a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a> &nbsp;&#124;&nbsp; <a href="https://tech.gov.sg/report_vulnerability" target="_blank">Report Vulnerability</a>',
+  });
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
