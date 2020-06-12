49aa7f08f55439875372fb129900cd96831702b7
Ryan Huang <ryanhuanglw@gmail.com>
2020-06-12 21:40:09 +0800

    Removed Ngx Runette Sidebar dependency as it's not working out. sigh...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,26 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { LeafletModule } from "@asymmetrik/ngx-leaflet";
-import { NgxSidebarControlModule } from "@runette/ngx-leaflet-sidebar";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
 import { MapLayerComponent } from './map/map-layer/map-layer.component';
-import { SidebarComponent } from './map/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MapComponent,
     MapLayerComponent,
-    SidebarComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
     LeafletModule,
-    NgxSidebarControlModule
   ],
   providers: [],
   bootstrap: [AppComponent]
