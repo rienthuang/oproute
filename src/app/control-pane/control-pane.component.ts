19710311357c0b8fc866cbdb594d439494c4e719
Ryan Huang <ryanhuanglw@gmail.com>
2020-06-13 17:34:49 +0800

    Developed click logic and styles for the control pane tabs

--- src/app/control-pane/control-pane.component.ts
+++ src/app/control-pane/control-pane.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faBars, faMap, faDirections } from "@fortawesome/free-solid-svg-icons";
+
 @Component({
   selector: 'app-control-pane',
   templateUrl: './control-pane.component.html',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ControlPaneComponent implements OnInit {
 
+  faBars = faBars;
+  faMap = faMap;
+  faDirections = faDirections;
+
+  activeTab = 'home'
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onTabClick(tabClicked: string) {
+    this.activeTab = tabClicked;
+
+    //Load panel component
+  }
+
+  isActiveIcon(icon: string) {
+    return icon === this.activeTab ? { color: 'white' } : {};
   }
 
 }
