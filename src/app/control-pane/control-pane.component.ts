8a4b594bffeabf45cce7793d1e68fde5381f9435
Ryan Huang <ryanhuanglw@gmail.com>
2020-06-13 18:11:30 +0800

    Set bottom icon to an information icon

--- src/app/control-pane/control-pane.component.ts
+++ src/app/control-pane/control-pane.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { faBars, faMap, faDirections, faLayerGroup, faMoon, faSun, faMountain, faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faMap, faDirections, faLayerGroup, faMoon, faSun, faMountain, faQuestionCircle, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-control-pane',
@@ -17,6 +17,7 @@ export class ControlPaneComponent implements OnInit {
   faSun = faSun;
   faMountain = faMountain;
   faQuestionCircle = faQuestionCircle;
+  faInfoCircle = faInfoCircle;
 
   activeTab = 'home'
 
