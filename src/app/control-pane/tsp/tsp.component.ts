ea618708466b582b7fcbe041a53fcba243ea86d5
Ryan Huang <ryanhuanglw@gmail.com>
2020-06-17 18:07:40 +0800

    Developed fly in and fly out animation when a Search component is created or removed

--- src/app/control-pane/tsp/tsp.component.ts
+++ src/app/control-pane/tsp/tsp.component.ts
@@ -3,12 +3,35 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { faCaretLeft, faCar, faBusAlt, faWalking, faRoute, faHome, faFlagCheckered } from "@fortawesome/free-solid-svg-icons";
 import { ControlPanelService } from 'src/app/services/control-panel.service';
 import { TspService } from 'src/app/services/tsp.service';
+import { trigger, style, state, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-tsp',
   templateUrl: './tsp.component.html',
   styleUrls: ['./tsp.component.css'],
-  providers: [TspService]
+  providers: [TspService],
+  animations: [
+    trigger('divFlyAnimation', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ])
+    ])
+  ]
 })
 export class TspComponent implements OnInit {
 
